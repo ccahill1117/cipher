397ee338d7e1e1503ec6678a20e7ef0483eebd75
Christopher Cahill and kohichi horii <kohichi@>
2018-10-15 16:42:12 -0700

    almost there

--- cipher/js/scripts.js
+++ cipher/js/scripts.js
@@ -8,10 +8,24 @@ var sentence = prompt("Enter your favorite color");
 
 var sentenceFirst = (sentence.replace(/^./, sentence.charAt(0).toUpperCase()));
 
+// var sentenceFirstLast = (sentence.replace(/^./, sentence.charAt(0).toUpperCase()))+
+// (sentence.replace(/.$/, sentence.charAt(sentenceLength - 1).toUpperCase()));
+
+
 alert(sentenceFirst);
 
 var sentenceLength = sentence.length;
 
-var sentenceLast = (sentence.replace(/.$/, sentence.charAt(sentenceLength - 1).toUpperCase()));
+// var sentenceLast = (sentence.replace(/.$/, sentence.charAt(sentenceLength - 1).toUpperCase()));
+
+var sentenceLast = (sentenceFirst.replace(/.$/, sentence.charAt(sentenceLength - 1).toUpperCase()));
 
 alert(sentenceLast);
+
+// var sentenceFirstLast = function (sentenceE) {
+//     sentenceE.replace(/^./, sentence.charAt(0).toUpperCase());
+//     sentenceE.replace(/.$/, sentence.charAt(sentenceLength - 1).toUpperCase());
+//     return sentenceE;
+// };
+//
+// alert(sentenceFirstLast(sentence));
